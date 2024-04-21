ec002b05f82fca41c14f41ff47b17e59e247b3c8
kawsar firoz <kawsarfiroz11@gmail.com>
2024-04-21 17:46:28 +0300

    Update CORS configuration

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const stripeRoute = require("./routes/stripeRoute");
 const bodyParser = require("body-parser")
 
 app.use(express.json());
-app.use(cors({ origin: ['https://ecommerce-client-wine.vercel.app'] }));
+app.use(cors({ origin: ['https://ecommerce-backend-three-omega.vercel.app/api/'] }));
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(cookieParser());
   
